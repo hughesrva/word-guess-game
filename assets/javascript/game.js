c3adc12aa0ac709e1a9d8dabfba423076e246666
skiplodem <hughesrva@gmail.com>
2018-11-18 20:15:02 -0500

    adding mostly done version of hw

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // list of words 
-var words = ["dog", "cat", "moose", "horse", "mouse", "elephant", "crocodile", "dingo", "hyena", "lion", "tiger", "bear"];
+var words = ["octorok", "guardian", "hyrule", "korok", "zelda", "ganon"];
 // selected word
 var selection;
 // list of letters in selected word
@@ -14,108 +14,156 @@ var guessesLeft;
 var guessed;
 // keyed letter
 var letter;
-// function to reset variable values
 
-// function to initialize variables and start game
+// function to hide element
+var hide = function (elementName) {
+    document.getElementById(elementName).style.visibility = "hidden";
+}
+
+// function to show element
+var show = function (elementName) {
+    document.getElementById(elementName).style.visibility = "visible";
+}
+
+// changes sound source before playing new sound
+var changeSound = function (soundSource) {
+    var sound = document.getElementById("sound");
+    sound.src = soundSource;
+}
+
+// hides all elements other than start screen
 var init = function () {
-    // show and set "word" to be the selection's letters replaced by underscores
-    var selection = words[Math.floor(Math.random() * words.length)];
-    var wordLetters = selection.split("");
-    var word = [];
-    for (let i = 0; i < wordLetters.length; i++) {
-        word.push("_");
-    }
-    var resetVariables = function () {
-        guessesLeft = 10;
-        guessed = [];
-        selection = words[Math.floor(Math.random() * words.length)];
-        wordLetters = selection.split("");
-        word = [];
-        console.log(word);
-        document.getElementById("word").innerHTML = word;
+    show("start");
+    hide("wordH");
+    hide("word");
+    hide("wins");
+    hide("winsH");
+    hide("guessesLeft");
+    hide("guessesLeftH");
+    hide("lettersGuessed");
+    hide("lettersGuessedH");
+    hide("gameOver1");
+    hide("gameOver2");
+
+    // function to create and show variables
+    var startGame = function () {
+        document.onclick = null;
+        // show and set "word" to be the selection's letters replaced by underscores
+        var selection = words[Math.floor(Math.random() * words.length)];
+        var wordLetters = selection.split("");
+        var word = [];
+        sound.pause();
+        song.volume = 0.3;
+        song.loop = true;
+        song.play();
         for (let i = 0; i < wordLetters.length; i++) {
             word.push("_");
         }
-        wins++;
-        document.getElementById("wins").innerHTML = wins;
-        console.log(wordLetters);
-    }
-    console.log(wordLetters);
-    document.getElementById("wordH").style.visibility = "visible";
-    document.getElementById("word").style.visibility = "visible";
-    document.getElementById("word").innerHTML = word.join(" ");
-
-    // show and set "wins"
-    wins = 0;
-    document.getElementById("wins").style.visibility = "visible";
-    document.getElementById("winsH").style.visibility = "visible";
-    document.getElementById("wins").innerHTML = wins;
-
-    // show and set "guessesLeft"
-    guessesLeft = 10;
-    document.getElementById("guessesLeft").style.visibility = "visible";
-    document.getElementById("guessesLeftH").style.visibility = "visible";
-    document.getElementById("guessesLeft").innerHTML = guessesLeft;
-
-    // show and set "lettersGuessed"
-    guessed = [];
-    document.getElementById("lettersGuessed").style.visibility = "visible";
-    document.getElementById("lettersGuessed").innerHTML = guessed;
-    document.getElementById("lettersGuessedH").style.visibility = "visible";
-
-    // hide "gameOver"
-    document.getElementById("gameOver").style.visibility = "hidden";
-
-    //logs keystrokes 
-    document.onkeyup = function (keyPress) {
-        //assigns keystrokes to "letter"
-        var letter = keyPress.key.toLowerCase();
-        // function to show game over screen and restart after keystroke
-        var gameOver = function () {
-            document.getElementById("word").style.visibility = "hidden";
-            document.getElementById("wins").style.visibility = "hidden";
-            document.getElementById("guessesLeft").style.visibility = "hidden";
-            document.getElementById("lettersGuessed").style.visibility = "hidden";
-            document.getElementById("wordH").style.visibility = "hidden";
-            document.getElementById("winsH").style.visibility = "hidden";
-            document.getElementById("guessesLeftH").style.visibility = "hidden";
-            document.getElementById("lettersGuessedH").style.visibility = "hidden";
-            document.getElementById("gameOver").style.visibility = "visible";
-            document.onkeyup = init;
-        }
-
-        // does nothing if key has already been tried
-        if (guessed.includes(letter)) {
-            console.log("You already guessed that!");
-        }
-        // adds letter to masked word if it matches
-        else if (wordLetters.includes(letter)) {
-            for (var j = 0; j < wordLetters.length; j++) {
-                if (letter === wordLetters[j]) {
-                    word[j] = letter;
-                }
-                if (word.indexOf("_") === -1) {
-                    resetVariables()
-                }
-                document.getElementById("word").innerHTML = word;
+        // resets most variables after completing word
+        var resetVariables = function () {
+            guessesLeft = 10;
+            guessed = [];
+            selection = words[Math.floor(Math.random() * words.length)];
+            wordLetters = selection.split("");
+            word = [];
+            console.log(word);
+            document.getElementById("word").innerHTML = word;
+            for (let i = 0; i < wordLetters.length; i++) {
+                word.push("_");
             }
+            wins++;
+            document.getElementById("wins").innerHTML = wins;
+            console.log(wordLetters);
         }
-        // adds key to tried list and deducts a guess if wrong
-        else if (guessesLeft > 1) {
-            guessed.push(letter);
-            document.getElementById("lettersGuessed").innerHTML = guessed;
-            guessesLeft--;
-            document.getElementById("guessesLeft").innerHTML = guessesLeft;
-        }
-        // resets if one guess remaining and guess was wrong
-        else {
-            gameOver();
+
+        show("title");
+        console.log(wordLetters);
+        wins = 0;
+        guessesLeft = 10;
+        guessed = [];
+        document.getElementById("lettersGuessed").innerHTML = guessed;
+        document.getElementById("word").innerHTML = word.join(" ");
+        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("guessesLeft").innerHTML = guessesLeft;
+        show("wordH");
+        show("word");
+        show("wins");
+        show("winsH");
+        show("guessesLeft");
+        show("guessesLeftH");
+        show("lettersGuessed");
+        show("lettersGuessedH");
+        hide("start");
+        hide("gameOver1");
+        hide("gameOver2");
+
+        //logs keystrokes 
+        document.onkeyup = function (keyPress) {
+            //assigns keystrokes to "letter"
+            var letter = keyPress.key.toLowerCase();
+            // function to show game over screen and restart after keystroke
+            var gameOver = function () {
+                hide("word");
+                hide("wins");
+                hide("guessesLeft");
+                hide("lettersGuessed");
+                hide("wordH");
+                hide("winsH");
+                hide("guessesLeftH");
+                hide("lettersGuessedH");
+                hide("title");
+                show("gameOver1");
+                show("gameOver2");
+                changeSound("assets/sounds/gameOver.mp3");
+                song.pause();
+                sound.play();
+                document.onkeyup = startGame;
+            }
+
+            // does nothing if key has already been tried
+            if (guessed.includes(letter)) {
+                console.log("You already guessed that!");
+            }
+            // adds letter to masked word if it matches
+            else if (wordLetters.includes(letter)) {
+                for (var j = 0; j < wordLetters.length; j++) {
+                    if (letter === wordLetters[j]) {
+                        word[j] = letter;
+                        changeSound("assets/sounds/itemget.mp3");
+                        sound.volume = 0.5;
+                        sound.play();
+                    }
+                    // checks to see if word is complete
+                    if (word.indexOf("_") === -1) {
+                        changeSound("assets/sounds/secretsolve.mp3");
+                        sound.play();
+                        resetVariables()
+                    }
+
+                    document.getElementById("word").innerHTML = word;
+                }
+            }
+            // adds key to tried list and deducts a guess if wrong
+            else if (guessesLeft > 1) {
+                changeSound("assets/sounds/korokdrop.mp3");
+                sound.volume = 0.5;
+                sound.play();
+                guessed.push(letter);
+                document.getElementById("lettersGuessed").innerHTML = guessed;
+                guessesLeft--;
+                document.getElementById("guessesLeft").innerHTML = guessesLeft;
+            }
+            // resets if one guess remaining and guess was wrong
+            else {
+                gameOver();
+            }
+
         }
 
     }
+    document.onclick = startGame;
 
 }
-
 
 
 
